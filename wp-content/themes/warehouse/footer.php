df7a96667342c98e67a269c624bdab9483105f87
Branndon Arruda <arruda.branndon@gmail.com>
2016-05-08 22:22:28 -0300

    Inserção de plugins, animações e html quase finalizado

---
+++ wp-content/themes/warehouse/footer.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains footer content and the closing of the
+ * #main div element.
+ *
+ * @package Odin
+ * @since 2.2.0
+ */
+?>
+
+	<footer id="footer" role="contentinfo">
+		<div class="container">
+			<div class="infos">
+				<div class="address col-md-4">
+					<h5>Onde Estamos:</h5>
+					<p><i class="fa fa-map-marker" aria-hidden="true"></i> Rua São José, 98 - Cubatão / SP</p>
+					<p><i class="fa fa-phone" aria-hidden="true"></i> (13) 3361-1919</p>
+					<p><a href="mailto:estudiowarehouse@gmail.com"><i class="fa fa-paper-plane" aria-hidden="true"></i> estudiowarehouse@gmail.com</a></p>
+					<p><i class="fa fa-skype" aria-hidden="true"></i> @estudiowarehouse</p>
+				</div>
+				<div class="smm col-md-4">
+					<h5>Siga-nos:</h5>
+					<a href="#" target="_blank" class="shrink"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+					<a href="#" target="_blank" class="shrink"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+					<a href="#" target="_blank" class="shrink"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
+				</div>
+			</div>
+
+			<div class="clear"></div>
+
+			<div class="copyright">
+				<p>© Ware House, <?php echo date( 'Y' ); ?>. All rights reserved. Design by Evelise</p>
+			</div>
+		</div>
+	</footer>
+
+	<!-- jQuery -->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script src="<?php bloginfo( 'template_directory' ); ?>/assets/js/functions.js"></script>
+
+	<?php wp_footer(); ?>
+</body>
+</html>
---
+++ wp-content/themes/warehouse/header.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till #main div
+ *
+ * @package Odin
+ * @since 2.2.0
+ */
+?><!DOCTYPE html>
+<html class="no-js" <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	<?php if ( ! get_option( 'site_icon' ) ) : ?>
+		<link href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.ico" rel="shortcut icon" />
+	<?php endif; ?>
+	<!--[if lt IE 9]>
+	<script src="<?php echo get_template_directory_uri(); ?>/assets/js/html5.js"></script>
+	<![endif]-->
+
+	<!-- Fontes -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css">
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
+
+	<!-- CSS - Default -->
+	<link rel="stylesheet" href="<?php bloginfo( 'template_directory' ); ?>/assets/css/warehouse.css" type="text/css" />
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+	<a id="skippy" class="sr-only sr-only-focusable" href="#content">
+		<div class="container">
+			<span class="skiplink-text"><?php _e( 'Skip to content', 'odin' ); ?></span>
+		</div>
+	</a>
+
+	<header id="header">
+		<div class="container">
+		<div id="logo" class="col-md-2">
+			<?php if (is_home()) : ?>
+				<img src="<?php bloginfo( 'template_directory' ); ?>/assets/images/logo.png" title="Warehouse - Gravação, Mix & master" alt="" />
+			<?php else : ?>
+				<a href="<?php bloginfo( 'url' ); ?>" title="<?php bloginfo( 'name' ); ?>" class="shrink">
+					<img src="<?php bloginfo( 'template_directory' ); ?>/assets/images/logo.png" title="Warehouse - Gravação, Mix & master" alt="" />
+				</a>
+			<?php endif; ?>
+		</div>
+
+		<div id="menu" class="col-md-6">
+			<ul>
+				<li>
+					<a href="#">Home</a>
+				</li>
+				<li>
+					<a href="#">Quem Somos</a>
+				</li>
+				<li>
+					<a href="#">Equipe</a>
+				</li>
+				<li>
+					<a href="#">Produções</a>
+				</li>
+				<li>
+					<a href="#">Contato</a>
+				</li>
+			</ul>
+		</div>
+		</div>
+	</header>
+
+
+
+
+
+
+
+
+
+
